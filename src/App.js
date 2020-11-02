f193a3b02534efb4c7363ce89289e6ff1e1221bc
amondragon <alex@rockmajesty.com>
2020-11-01 18:35:41 -0600

    adding mock data and inbox component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import Inbox from "./components/Inbox";
 
 export default function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/">
+            <Inbox />
+          </Route>
+        </Switch>
+      </div>
+      <Redirect exact from="/" to="/inbox" />
+    </Router>
+  );
 }
---
+++ src/components/Inbox.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Inbox() {
+  return <React.Fragment>Inbox</React.Fragment>;
+}
